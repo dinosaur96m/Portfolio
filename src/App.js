98b66ddeea03828daf7f7499ebbdc2c668fcefa8
dinosaur96m <nccadmantinaos@gmail.com>
2021-12-22 00:27:05 -0500

    left pannel and content stays fixed while right scrolls'

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header.js'
 import Project from './components/Project'
 import Footer from './components/Footer'
 
+
 function App() {
 
   const projectOne = {
--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,32 @@
+import { StickyContainer, Sticky } from "react-sticky"
 
 function About() {
     return (
+        <StickyContainer>
         <div className="about">
-            <h1>Dino Mantinaos</h1>
-            <div className="intro">
-                Hi there, I'm Dino, a Brooklyn based Software Developer. Thanks for taking the time to learn more about me and my work. I'd love to hear from you!
-            </div>
-            <div className="skills">
-                <h2>Technical Skills</h2>
-                <span>MongoDB, Express.JS, React.JS, Node.JS</span>
-                <span>HTML, CSS, JavaScript</span>
-                <span>PostgresSQL, Sequelize, Git, GitHub, Bootstrap</span>
-            </div>
-            <div className="resume">
-                <h1>RESUME</h1>
-            </div>
+            <Sticky> 
+                { () => {
+                return (
+                    <div className="aboutContent">
+                        <h1>Dino Mantinaos</h1>
+                        <div className="intro">
+                            Hi there, I'm Dino, a Brooklyn based Software Developer. Thanks for taking the time to learn more about me and my work. I'd love to hear from you!
+                        </div>
+                        <div className="skills">
+                            <h2>Technical Skills</h2>
+                            <span>MongoDB, Express.JS, React.JS, Node.JS</span>
+                            <span>HTML, CSS, JavaScript</span>
+                            <span>PostgresSQL, Sequelize, Git, GitHub, Bootstrap</span>
+                        </div>
+                        <div className="resume">
+                            <h1>RESUME</h1>
+                        </div>
+                    </div>
+                    )
+                }
+            }</Sticky>
         </div>
+        </StickyContainer>
     )
 }
 
